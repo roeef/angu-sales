ff1fbcffc08ec2aae7f88906cf20d2aee2d97193
roeef <roeef@mail-wise.com>
2018-02-22 10:32:54 +0200

    dogs becomes sales

---
+++ src/app/customer-row/customer-row.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustomerRowComponent } from './customer-row.component';
+
+describe('CustomerRowComponent', () => {
+  let component: CustomerRowComponent;
+  let fixture: ComponentFixture<CustomerRowComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CustomerRowComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CustomerRowComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/customer-row/customer-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-row',
+  templateUrl: './customer-row.component.html',
+  styleUrls: ['./customer-row.component.css']
+})
+export class CustomerRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
